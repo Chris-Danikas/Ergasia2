c1bf6313c57d62d61194b82c2282f54a1449abaa
Chris-Danikas <christosda14@gmail.com>
2023-11-15 21:32:31 +0200

    c2 - Read and display 5 items from data.txt

--- rainfall.f95
+++ rainfall.f95
@@ -1,3 +1,23 @@
 program rainfall
-
-end program
+    implicit none
+    character(20) :: polh(5)
+    integer :: vroxh(5)
+    integer :: i
+ 
+    open(1, file='data.txt', status='old')
+ 
+    ! Διάβασμα της πρώτης γραμμής (τίτλων)
+    read(1, *)
+ 
+    ! Διάβασμα δεδομένων και αποθήκευση στους πίνακες
+    do i = 1, 5
+       read(1, *) polh(i), vroxh(i)
+    end do
+ 
+    close(1)
+ 
+    ! Εκτύπωση περιεχομένων πινάκων
+    do i = 1, 5
+       print *, polh(i), vroxh(i)
+    end do
+ end program rainfall
